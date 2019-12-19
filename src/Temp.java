5fc11eb096478d7e845beac0635a1a0072c5c429
刘铂园 <1027159514@qq.com>
2019-12-19 10:24:01 +0800

    222

--- src/Temp.java
+++ src/Temp.java
@@ -1,6 +1,6 @@
 public class Temp {
     public static void main(String[] args) {
-        System.out.println("111111");
-        System.out.println("2222222");
+        System.out.println("11111122222222");
+
     }
 }
